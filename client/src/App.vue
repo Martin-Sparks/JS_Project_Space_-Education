c43695c251abb1ef56c5ceeaaa69d98ce4d49ffe
Martin-Sparks <martin.sparks82@gmail.com>
2020-05-22 15:50:12 +0100

    API data flow confirmed

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  
+  <div id="app">
+    <h1>Journey Around the Solar System </h1>
+    <p>{{ destinations }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'app',
+  data: function () {
+    return {
+      destinations: []
+    }
+  },
+  mounted: function () {
+    fetch('https://api.le-systeme-solaire.net/rest/bodies/')
+      .then(res => res.json())
+      .then(data => this.destinations = data);
+  }
 }
 </script>
 
