2b05020501ff14e45654f8df041a0f3ec28f3715
Martin-Sparks <martin.sparks82@gmail.com>
2020-05-22 16:28:50 +0100

    computed destination added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="app">
-    <h1>Journey Around the Solar System </h1>
-    <p>{{ destinations }}</p>
+    <h1>Journey Around the Solar System</h1>
+    <journey-form :destinations="destinations"></journey-form>
   </div>
 </template>
 
 <script>
+import JourneyForm from "@/components/JourneyForm.vue"
+
 export default {
-  name: 'app',
-  data: function () {
+  name: "app",
+  data: function() {
     return {
       destinations: []
-    }
+    };
   },
-  mounted: function () {
-    fetch('https://api.le-systeme-solaire.net/rest/bodies/')
+  mounted: function() {
+    fetch("https://api.le-systeme-solaire.net/rest/bodies/")
       .then(res => res.json())
-      .then(data => this.destinations = data);
+      .then(data => (this.destinations = data.bodies));
+  },
+  components: {
+    "journey-form": JourneyForm
   }
-}
+};
 </script>
 
 <style>
-
 </style>
---
+++ client/src/components/JourneyForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <form>
+            <label for="selected">Select a destination: </label>
+            <input type="text" name="selected" id="selected" list="destinations" v-model="selectedDestinationName" />
+            <datalist id="destinations">
+                <option v-for="(destination, index) in destinations" :key="index" :value="destination.englishName">{{ destination.englishName }}</option>
+            </datalist>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'journey-form',
+    props: ['destinations'],
+    data: function () {
+        return {
+            selectedDestinationName: ""
+        }
+    },
+    computed: {
+        selectedDestination: function () {
+            return this.destinations.find(destination => destination.englishName === this.selectedDestinationName);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
