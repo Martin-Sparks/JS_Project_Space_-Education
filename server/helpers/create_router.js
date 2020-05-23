e2ce7435711831625eecb94b5bd3dd0ef2b7c2a3
Martin-Sparks <martin.sparks82@gmail.com>
2020-05-23 13:57:00 +0100

    all crud routes added

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -63,7 +63,21 @@ const createRouter = function (collection) {
       });
   });
 
+  //SHOW
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectID(id) })
+      .then((result) => res.json(result))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
   return router;
+
 };
 
 module.exports = createRouter;
