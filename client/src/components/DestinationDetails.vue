64388ff780979ffa51a6a1976925bab33e1beda9
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-23 13:47:46 +0100

    add ability to add a destination to a journey

---
+++ client/src/components/DestinationDetails.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{englishName}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "destination-details",
+    props: ["destination"],
+    data: function(){
+        return{
+            englishName: this.destination.englishName
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
