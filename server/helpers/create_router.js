42a3bbe123f44d71b634cea6ec6b2320986d3d7c
Martin-Sparks <martin.sparks82@gmail.com>
2020-05-23 11:30:06 +0100

    Create and Index created

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,10 +1,38 @@
-const express = require('express');
-const ObjectID = require('mongodb').ObjectID;
+const express = require("express");
+const ObjectID = require("mongodb").ObjectID;
 
 const createRouter = function (collection) {
-    const router = express.Router();
+  const router = express.Router();
 
-    return router;
+  // INDEX
+  router.get("/", (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
+
+  // CREATE
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection.insertOne(newData)
+      .then((result) => {
+          res.json(result.ops[0])
+        })
+     .catch(err => {
+        error(err);
+        res.status(500);
+        res.json({status: 500, error: err});
+    });
+});
+
+return router;
+
 };
 
 module.exports = createRouter;
