dae562cde2936e58fdd9335006831905563d3d14
cjscrimmy <craigscrimgeour@yahoo.com>
2020-05-26 12:44:13 +0100

    add gauge but not working

--- client/src/components/FuelGauge.vue
+++ client/src/components/FuelGauge.vue
@@ -1,10 +1,41 @@
-<template>
-  <p>Hello!</p>
+<template lang="html">
+    <div>
+        <GChart
+            :settings="{ packages: ['gauge'] }"
+            type="Gauge"
+            :data="chartData"
+            :options="chartOptions"
+        />
+    </div>
 </template>
 
 <script>
-export default {
+import { GChart } from 'vue-google-charts'
 
+export default {
+    name: 'fuel-gauge',
+    props: ['fuel'],
+    data: function () {
+        return {
+            chartOptions: {
+                chart: {
+                    title: 'Company Performance',
+                    subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                }
+            }
+        }
+    },
+    computed: {
+        chartData: function () {
+            return [
+                ['Label', 'Value'],
+                ['Fuel', this.fuel]
+            ];
+        }
+    },
+    components: {
+        GChart
+    }
 }
 </script>
 
